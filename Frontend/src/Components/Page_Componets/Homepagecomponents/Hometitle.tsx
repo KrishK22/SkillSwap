75f7364579977cd138276455a61be15617a09d87
Parth Gupta <guptaparth1304@gmail.com>
2025-07-12 12:23:20 +0530

    Card

--- Frontend/src/Components/Page_Componets/Homepagecomponents/Hometitle.tsx
+++ Frontend/src/Components/Page_Componets/Homepagecomponents/Hometitle.tsx
@@ -1,15 +1,18 @@
-import { Button } from "@/Components/ui/button";
+import { Button } from "@/Components/ui/button.tsx";
 
 const Hometitle = () => {
   return (
-    <div className="flex mr-18 bg-gray-200 justify-between items-center px-6 py-8 ">
-      <div className="logo text-indigo-600 font-bold text-4xl">LOGO</div>
+    <div className="flex   justify-between items-center px-6 py-8 ">
+      <div className="logo text-black font-bold text-4xl"></div>
       <div className="flex">
-        <Button variant={"outline"} className=" mr-3"size={"lg"}>
-          Home
+        <img src="/assets/logo.svg" alt="" />
+        <Button
+          variant={"ghost"}
+          className=" text-black text-xl mr-3"
+          size={"sm"}
+        >
+          Swap Requests
         </Button>
-        {/* <h1 className="ml-2 mr-2  text-4xl ">/</h1> */}
-        <Button variant={"outline"} size={"lg"}>Login</Button>
       </div>
     </div>
   );
